d62fd6f0a645eb519ca1b0ca89e5143f281b8395
joshmoritty <josh.moritty@gmail.com>
2026-02-10 20:25:09 +0700

    style: lint and format

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
 
 import js from '@eslint/js';
 import globals from 'globals';
@@ -9,30 +9,36 @@ import prettier from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
-export default defineConfig([globalIgnores(['dist']), {
-  files: ['**/*.{js,jsx}'],
-  extends: [
-    js.configs.recommended,
-    reactHooks.configs.flat.recommended,
-    reactRefresh.configs.vite,
-  ],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: {
-      ...globals.browser,
-      ...globals.node, // For global
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        ...globals.node, // For global
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
     },
-    parserOptions: {
-      ecmaVersion: 'latest',
-      ecmaFeatures: { jsx: true },
-      sourceType: 'module',
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'prettier/prettier': 'error',
     },
   },
-  plugins: {
-    prettier,
-  },
-  rules: {
-    'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    'prettier/prettier': 'error',
-  },
-}, prettierConfig, ...storybook.configs["flat/recommended"], ...storybook.configs["flat/recommended"]]);
+  prettierConfig,
+  ...storybook.configs['flat/recommended'],
+  ...storybook.configs['flat/recommended'],
+]);
